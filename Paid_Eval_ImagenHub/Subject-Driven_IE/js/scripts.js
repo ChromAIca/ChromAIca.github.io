3112637b3648a5a0b73d05a34de11a2539c67274
Max Ku <m3ku@uwaterloo.ca>
2023-09-22 01:57:11 -0400

    up

---
+++ Paid_Eval_ImagenHub/Subject-Driven_IE/js/scripts.js
@@ -0,0 +1,9 @@
+// Empty JS for your own code to be here
+function hide_and_show() {
+    var x = document.getElementById("humaneval_examples");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
