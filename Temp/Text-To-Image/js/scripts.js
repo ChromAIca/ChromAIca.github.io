a8c56219178fe7e8229d2e008ae9f8ec7a6e8bf9
Max Ku <m3ku@uwaterloo.ca>
2023-11-16 03:26:41 -0500

    remove exp

--- Temp/Text-To-Image/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Empty JS for your own code to be here
-function hide_and_show() {
-    var x = document.getElementById("humaneval_examples");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
